97fa5257201091b6cab3e26e64c46fa86d766748
Volodimir Petrash <wofer151@gmail.com>
2023-06-04 23:17:02 +0300

    Privacy + Terms og Usage

--- InstanceScans/Controllers/HomeController.cs
+++ InstanceScans/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace InstanceScans.Controllers
         {
             return View();
         }
+        public IActionResult TermsOfUsage() 
+        { 
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
